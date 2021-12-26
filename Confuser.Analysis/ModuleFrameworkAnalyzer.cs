d36737b24aa27026c339ec43f069a0fbb7e317f2
Martin Karing <karing.martin@gmail.com>
2021-12-26 16:14:52 +0100

    tmp

---
+++ Confuser.Analysis/ModuleFrameworkAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dnlib.DotNet;
+
+namespace Confuser.Analysis {
+	/// <summary>
+	/// This class contains everything currently implemented to identify the framework version of an specific module.
+	/// </summary>
+	internal static class ModuleFrameworkAnalyzer {
+		private const string _tfmFramework = ".NETFramework";
+		private const string _tfmUwp = ".NETCore";
+		private const string _tfmCore = ".NETCoreApp";
+		private const string _tfmStandard = ".NETStandard";
+
+		internal static (ModuleFramework Framework, Version? Version) IdenitfyFramework(ModuleDef moduleDef) {
+			if (moduleDef is null) throw new ArgumentNullException(nameof(moduleDef));
+
+
+			moduleDef.Assembly.TryGetOriginalTargetFrameworkAttribute
+		}
+
+		private static ModuleFramework TryIdentityByAttribute(ModuleDef moduleDef, out Version? version) {
+			AssemblyDef asmDef = moduleDef.Assembly;
+			if (asmDef is null || !asmDef.TryGetOriginalTargetFrameworkAttribute(out string frameworkMoniker, out version, out _)) {
+				version = null;
+				return ModuleFramework.Unknown;
+			}
+
+			switch (frameworkMoniker) {
+				case _tfmFramework:	return ModuleFramework.DotNetFramework;
+				case _tfmCore: return ModuleFramework.DotNet;
+				case _tfmStandard: return ModuleFramework.DotNetStandard;
+				case _tfmUwp: return ModuleFramework.Uwp;
+			}
+
+			return ModuleFramework.Unknown;
+		}
+	}
+}
---
+++ Confuser.Core/Frameworks/DotNetFrameworkDiscovery.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.Win32;
+
+namespace Confuser.Core.Frameworks {
+	internal sealed class DotNetFrameworkDiscovery : IFrameworkDiscovery {
+		private List<IInstalledFramework> InstalledFrameworks { get; set; }
+
+		public IEnumerable<IInstalledFramework> GetInstalledFrameworks()
+			=> InstalledFrameworks ??= DiscoverFrameworks().ToList();
+
+		private static IEnumerable<IInstalledFramework> DiscoverFrameworks() {
+			if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+				yield break;
+
+			// http://msdn.microsoft.com/en-us/library/hh925568.aspx
+			using (RegistryKey ndpKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\")) {
+				foreach (string versionKeyName in ndpKey.GetSubKeyNames()) {
+					if (!versionKeyName.StartsWith("v"))
+						continue;
+					using RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+					var name = (string)versionKey.GetValue("Version", "");
+					string sp = versionKey.GetValue("SP", "").ToString();
+					string install = versionKey.GetValue("Install", "").ToString();
+					if (install == "" || sp != "" && install == "1")
+						yield return versionKeyName + "  " + name;
+
+					if (name != "")
+						continue;
+
+					foreach (string subKeyName in versionKey.GetSubKeyNames()) {
+						RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+						name = (string)subKey.GetValue("Version", "");
+						if (name != "")
+							sp = subKey.GetValue("SP", "").ToString();
+						install = subKey.GetValue("Install", "").ToString();
+
+						if (install == "")
+							yield return versionKeyName + "  " + name;
+						else if (install == "1")
+							yield return "  " + subKeyName + "  " + name;
+					}
+				}
+			}
+
+			using (RegistryKey ndpKey =
+				RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "")
+					.OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\")) {
+				if (ndpKey.GetValue("Release") == null)
+					yield break;
+				var releaseKey = (int)ndpKey.GetValue("Release");
+				yield return "v4.5 " + releaseKey;
+			}
+		}
+
+		private sealed class InstalledDotNetFramework : IInstalledFramework {
+
+		}
+	}
+}
